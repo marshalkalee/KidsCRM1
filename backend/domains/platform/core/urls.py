d0cc422e8b0bd7c410ef8dfbef3a7681267fed4c
Darya <briliantik795@gmail.com>
2026-09-16 14:28:06 +0500

    fix: add img attributes for djlint

--- backend/domains/platform/core/urls.py
+++ backend/domains/platform/core/urls.py
@@ -6,4 +6,5 @@ app_name = "core"
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("login/", views.login_view, name="login"),
 ]
--- backend/domains/platform/core/views.py
+++ backend/domains/platform/core/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "platform/home.html")
+
+
+def login_view(request):
+    return render(request, "auth/login.html")
